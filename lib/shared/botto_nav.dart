bf652f06adb218c2f57906cc3e3b0ed2ec4b0c9c
Joao Pedro Jacques Hoss <joaopedro_jh@hotmail.com>
2023-06-14 11:20:12 -0300

    refac: now uses a selected index provider and show primary color

--- lib/shared/botto_nav.dart
+++ lib/shared/botto_nav.dart
@@ -1,37 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({super.key});
+final selectedIndexBottomNavBarProvider = StateProvider<int>((ref) => 0);
+
+class BottomNavBar extends ConsumerWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentIndex =
+        ref.watch(selectedIndexBottomNavBarProvider.notifier).state;
+
     return BottomNavigationBar(
+      currentIndex: currentIndex,
       items: const [
+        // BottomNavigationBarItem(
+        //   icon: Icon(
+        //     FontAwesomeIcons.truckFast,
+        //     size: 20,
+        //   ),
+        //   label: 'Corridas',
+        // ),
+        // BottomNavigationBarItem(
+        //   icon: Icon(
+        //     FontAwesomeIcons.boxesPacking,
+        //     size: 20,
+        //   ),
+        //   label: 'Entregas',
+        // ),
         BottomNavigationBarItem(
           icon: Icon(
-            FontAwesomeIcons.map,
+            FontAwesomeIcons.solidMap,
             size: 20,
           ),
-          label: 'Map',
+          label: 'Mapa',
         ),
         BottomNavigationBarItem(
           icon: Icon(
-            FontAwesomeIcons.user,
+            FontAwesomeIcons.solidUser,
             size: 20,
           ),
-          label: 'Profile',
+          label: 'Perfil',
         ),
       ],
-      fixedColor: Colors.deepPurple[200],
+      selectedItemColor: Theme.of(context)
+          .primaryColor, // Use the primary color of your theme for highlighting
+      unselectedItemColor: Colors.grey, // Adjust the color for unselected items
       onTap: (int index) {
+        ref.read(selectedIndexBottomNavBarProvider.notifier).state =
+            index; // Update the selected index
         switch (index) {
           case 0:
             Navigator.pushNamed(context, '/map');
             break;
           case 1:
             Navigator.pushNamed(context, '/profile');
-            break;                          
+            break;
         }
       },
     );
